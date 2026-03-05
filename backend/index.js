9139a07c31d6293c4cc6b758cb566bfc826b9736
rehanjaswal <rehanxjaswal@gmail.com>
2026-03-05 20:32:32 +0530

    feat: express server, auth routes, controller structure, prisma, postman setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import authRoutes from "./routes/auth.js"
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.json({message : "ARX is alive"});
